8b0f4c417d778d5db31c6a5d43cf3e838d348c40
Serge Pokhodyaev <snp@z9.ru>
2017-02-23 21:19:07 +0300

    detect package name

--- jenkins-pbuilder-helper.rb
+++ jenkins-pbuilder-helper.rb
@@ -5,9 +5,11 @@ require_relative 'lib/aptly'
 require_relative 'lib/pbuilder'
 require_relative 'lib/git_info'
 require_relative 'lib/erb_renderer'
+require_relative 'lib/notify'
 
 # ==== 1. check, collect and prepare all needed info =========================
 MyEnsure.dir('debian/')
+MyEnsure.file('debian/control')
 
 pbuilder = Pbuilder.new
 
@@ -17,7 +19,12 @@ aptly.ensure_repo
 commit = GitInfo.new
 
 build_number = MyEnsure.env('BUILD_NUMBER')
-pkg_name = 'npoed-sso-edx'
+# FIXME: use readable code
+if File.readlines('debian/control').find { |ln| ln =~ %r{^Package:\s+} }.to_s =~ %r{^Package:\s+(\S+)$}
+  pkg_name = $1
+else
+  Notify.fatal("can't find package name")
+end
 pkg_version = [commit.time, commit.hash, build_number].join('~')
 pkg_arch = 'amd64'
 
