60e846ebbd86d1f0208802ff21e1ec1cf5c20fab
Serge Pokhodyaev <snp@z9.ru>
2017-02-23 21:49:23 +0300

    rewrite using MyPackage class

--- jenkins-pbuilder-helper.rb
+++ jenkins-pbuilder-helper.rb
@@ -1,49 +1,8 @@
 #!/usr/bin/env ruby
 
-require_relative 'lib/my_ensure'
-require_relative 'lib/aptly'
-require_relative 'lib/pbuilder'
-require_relative 'lib/git_info'
-require_relative 'lib/erb_renderer'
-require_relative 'lib/notify'
+require_relative 'lib/my_package'
 
-# ==== 1. check, collect and prepare all needed info =========================
-MyEnsure.dir('debian/')
-MyEnsure.file('debian/control')
-
-pbuilder = Pbuilder.new
-
-aptly = Aptly.new(MyEnsure.env('aptly_prefix'), MyEnsure.env('aptly_distribution'))
-aptly.ensure_repo
-
-commit = GitInfo.new
-
-build_number = MyEnsure.env('BUILD_NUMBER')
-# FIXME: use readable code
-if File.readlines('debian/control').find { |ln| ln =~ %r{^Package:\s+} }.to_s =~ %r{^Package:\s+(\S+)$}
-  pkg_name = $1
-else
-  Notify.fatal("can't find package name")
-end
-pkg_version = [commit.time, commit.hash, build_number].join('~')
-pkg_arch = 'amd64'
-
-# we need to copy ssh key since we going to use chroot environment to build
-if ENV.has_key?('git_ssh_key')
-  MyEnsure.install(ENV['git_ssh_key'], "debian/git_ssh_key", :mode => 0600, :verbose => true)
-end
-
-# ==== 2. generate changelog =================================================
-ERBRenderer.new('debian/changelog').generate({
-  :version         => pkg_version,
-  :date            => commit.rfc_date,
-  :changelog_lines => commit.changelog_lines,
-})
-
-# ==== 3. build ==============================================================
-pbuilder.run(aptly.distribution)
-
-# ==== 4. publish to repository ==============================================
-result = "#{pbuilder.cache}/result/#{pkg_name}_#{pkg_version}_#{pkg_arch}.deb"
-MyEnsure.file(result)
-aptly.add(result)
+pkg = MyPackage.new
+pkg.prepare
+pkg.build
+pkg.publish
---
+++ lib/my_package.rb
@@ -0,0 +1,72 @@
+require_relative 'my_ensure'
+require_relative 'pbuilder'
+require_relative 'git_info'
+require_relative 'aptly'
+
+class MyPackage
+  def initialize
+    validate_dpkg_files
+
+    @pbuilder = Pbuilder.new
+
+    @aptly = Aptly.new(MyEnsure.env('aptly_prefix'), MyEnsure.env('aptly_distribution'))
+    @aptly.ensure_repo
+
+    @build_number = MyEnsure.env('BUILD_NUMBER')
+    @commit = GitInfo.new
+
+    @pkg_name = get_pkg_name
+    @pkg_version = get_pkg_version
+    @pkg_arch = get_pkg_arch
+  end
+
+  def prepare
+    # we need to copy ssh key since we going to use chroot environment to build
+    if ENV.has_key?('git_ssh_key')
+      MyEnsure.install(ENV['git_ssh_key'], "debian/git_ssh_key", :mode => 0600, :verbose => true)
+    end
+
+    ERBRenderer.new('debian/changelog').generate({
+      :version         => @pkg_version,
+      :date            => @commit.rfc_date,
+      :changelog_lines => @commit.changelog_lines,
+    })
+  end
+
+  def build
+    @pbuilder.run(@aptly.distribution)
+  end
+
+  def publish
+    result = File.join(@pbuilder.cache,
+                       'result',
+                       "#{@pkg_name}_#{@pkg_version}_#{@pkg_arch}.deb")
+    MyEnsure.file(result)
+    @aptly.add(result)
+  end
+
+  private
+
+  def validate_dpkg_files
+    MyEnsure.dir('debian/')
+    MyEnsure.file('debian/control')
+  end
+
+  def get_pkg_name
+    pkg_line = File.readlines('debian/control').find { |ln| ln =~ %r{^Package:\s+} }.to_s
+
+    unless pkg_line =~ %r{^Package:\s+(\S+)$}
+      Notify.fatal("can't find package name")
+    end
+
+    $1
+  end
+
+  def get_pkg_version
+    [@commit.time, @commit.hash, @build_number].join('~')
+  end
+
+  def get_pkg_arch
+    'amd64'   # FIXME
+  end
+end
