5325cf37a5d545fc1ecd0957bc4e545e3b8546e1
Serge Pokhodyaev <snp@z9.ru>
2017-03-07 20:42:00 +0300

    allow to execute separate steps

--- jenkins-pbuilder-helper.rb
+++ jenkins-pbuilder-helper.rb
@@ -3,6 +3,12 @@
 require_relative 'lib/my_package'
 
 pkg = MyPackage.new
-pkg.prepare
-pkg.build
-pkg.publish
+arg = ARGV[0]
+
+unless arg.nil?
+  pkg.send(arg)
+else
+  pkg.prepare
+  pkg.build
+  pkg.publish
+end
